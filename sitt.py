4a83989550e42bd605abdc30d298410b6b482095
Mark Carter <mcturra2000@yahoo.co.uk>
2010-11-06 10:40:07 +0000

    out-of-control-mods

--- sitt.py
+++ sitt.py
@@ -1,13 +1,47 @@
 '''sitt.py - Some Interesting Thimbl Tools
 '''
 
+import cStringIO
 import json
 import optparse
 import os
 import re
 import subprocess
+import sys
 import time
 
+stack = []
+
+
+def piping(cmd):
+    global stack
+    stack.append(sys.stdin.read())
+    eval(cmd +'()')
+    print stack.pop()
+    
+def explan():
+    '''Extract the plan from the input'''
+    global stack
+    m = re.search('^.*?Plan:\s*(.*)', stack.pop(), re.M + re.S)
+    stack.append(m.group(1))
+
+def jencode():
+    '''Encode top of stack to json'''
+    global stack
+    t1 = stack.pop()
+    t2 = json.loads(t1)
+    stack.append(t2)
+
+def jextract():
+    '''Extract a key from a json object on the stack'''
+    global stack
+    key = stack.pop()
+    j = stack.pop()
+    #print t1
+    t2 = json.dumps(t1, indent = 4)
+    stack.append(t2)
+    
+    
 def finger_user(user_name):
     '''Try to finger a user, and convert the returned plan into a dictionary
     E.g. j = finger_user("dk@telekommunisten.org")
@@ -50,17 +84,25 @@ def add_message(text):
     file(plan_filename, 'w').write(plan_text)
     print "OK"
 
+def follow():
+    print "FIXME you're following me"
+    
 def main():
     parser = optparse.OptionParser()
     parser.add_option("-m", "--message", dest="message", help="add a message")
     parser.add_option("-a", "--address", dest="address", help="pretty-print the plan file for an address")
+    parser.add_option("-f", "--follow", action="store_true", dest="follow", help="follow someone")
+    parser.add_option("-p", "--pipe", dest="pipe", help="pipe a command through stdin/stdout")
     (options, args) = parser.parse_args()
     #print options
     #print args
     if options.message:
         add_message(options.message)
     if options.address:
-        single_finger(options.address)                    
+        single_finger(options.address)
+    if options.follow:
+        follow()
+    if options.pipe: piping(options.pipe)
     
 if __name__ == "__main__":
     main()
