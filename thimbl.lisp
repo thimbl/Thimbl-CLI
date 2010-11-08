c383ad260134003dc2b3c5118dbd07d84051a9bd
Mark Carter <mcturra2000@yahoo.co.uk>
2010-11-08 17:59:49 +0000

    General mods - bit of a mess, actually

--- thimbl.lisp
+++ thimbl.lisp
@@ -1,6 +1,89 @@
 (ql:quickload "cl-json")
 
-(defvar *plans* nil "All the plans: us and others")
+(defclass collection () 
+  ((cursor :initform 0)
+   (contents :initform (make-array 2 :fill-pointer 0 :adjustable t))))
+
+(defun do-collection (coll function)
+  "For each item in collection COLL, call FUNCTION on it"
+  (loop for el across (slot-value coll 'contents) do
+        (funcall function  el)))
+
+(defun insert (collection value)
+  "Add a value to a collection"
+  (vector-push-extend value (slot-value collection 'contents)))
+  
+(defclass followee ()
+  ((nick :initarg :nick)
+   (address :initarg :address)))
+
+(defclass post () (to text time))
+
+(defclass plan () 
+  ((address :initform "--VERY IMPORTANT--")
+   (name :initform "ANON")
+   (bio :initform "Just a guy")
+   (website :initform "www.example.com")
+   (email :initform "foo@example.com")
+   (mobile :initform "")
+   (following :initform (make-instance 'collection))
+   (posts :initform (make-instance 'collection))))
+
+(defvar *me* nil "My plan")
+(defun reset-me ()(setf *me* (make-instance 'plan)))
+(unless *me* (reset-me))
+
+(defun post (text)
+  "Post TEXT"
+  (insert (slot-value *me* 'posts) text))
+
+(post "hello world")
+
+(defun follow (nick address)
+  "Follow someone with nickname NICK and finger address ADDRESS"
+  (insert (slot-value *me* 'following) 
+          (make-instance 'followee :nick nick :address address)))
+
+(defgeneric display (object)
+  (:documentation "Generic method for displaying an object"))
+
+(defmethod display ((f followee))
+  (format t "~a~10t~a ~%" 
+          (slot-value f 'nick) 
+          (slot-value f 'address)))
+
+(defmethod display ((p plan))
+  (format t "~a~10t~a ~%" 
+          (slot-value p 'bio) 
+          (slot-value p 'address)))
+
+
+(defun following ()
+  "Print information about who I am following"
+  (do-collection (slot-value *me* 'following) #'display))
+
+
+
+(defun plans ()
+  "Print information about the plans in the database"
+  (do-collection *plans* #'display))
+
+;(setf f (make-instance 'followee :nick "foo" :address "bar"))
+(follow "mike" "mike@mikepearce.net")
+(follow "dk" "dk@telekommunisten.org")
+(follow "ashull" "ashull@telekommunisten.org")
+(following)
+(plans)
+
+
+(defvar *plans* nil "All the plans: including me")
+(defun reset-plans () 
+  (setf *plans* (make-instance 'collection))
+  (insert *plans* *me*))
+
+(unless *plans* (reset-plans))
+
+
 
 (defun run-finger (user)
   (run-program "finger" 
@@ -33,21 +116,8 @@
 
 (defun get-value (symbol list)
   "Get a value associated with a list"
-  (cdr (assoc symbol list)))
+  (assoc symbol list))
 
-(defun print-followings (plan)
-  "Print bio, name and properties associated with a plan"
-  (terpri)
-  (let ((fmt "~5a ~10a ~20a~%"))
-    (format t fmt "IDX" "NICK" "ADDRESS")
-    (loop with idx = 0 with nick with address 
-          for followee in (get-value :following plan) do
-        
-          (incf idx)
-          (setf nick (get-value :nick followee))
-          (setf address (get-value :address followee))
-          (format t fmt idx nick address)))
-  (terpri))
 
 
 (defun print-message (address message)
@@ -63,7 +133,7 @@
 
 (defun plan-key (plan)
   "Return a unique key identifying a plan"
-  (get-value :email (get-value :properties plan)))
+  (getf plan :address))
 
 (defun print-messages ( &optional (plans *plans*))
   "Print all the messages in all PLANS. 
@@ -74,12 +144,29 @@ Use global plans if argument not specified"
               for message in (get-value :messages  plan) do
               (print-message address message))))
 
-(defun store-plan-into-global (plan)
+(defun store-plan (plan)
   "Insert/replace a plan into the global plans collection"
   (pushnew plan *plans* :key #'plan-key))
 
-;(setf json (finger-to-json "dk@telekommunisten.org"))
-(print-followings (first *plans*))
-(print-messages *plans*)
-; (finger-to-json "foo")
+
+(defun address-lambda (address)
+  (lambda (x) (equalp (plan-key x) address)))
+
+(defun drop-plan (address)
+  (setf *plans* (remove-if (address-lambda address) *plans*))
+  (when (equalp *me* address)
+    (setf *me* nil)))
+
+(defun find-plan (address)
+  (find-if (address-lambda address) *plans*))
+
+(defun fetch (address)
+  "Finger ADDRESS, and add the information to the plans"
+  "FIXME")
+  
+
+  
+(setf json (finger-to-json "dk@telekommunisten.org"))
 ; (saveinitmem)
+(setf c (make-instance 'collection))
+(add c 42)
