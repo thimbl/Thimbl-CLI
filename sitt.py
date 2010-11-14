342662f5fcded2d0636e7e0069b475840188d344
Mark Carter <mcturra2000@yahoo.co.uk>
2010-11-14 19:54:19 +0000

    Changes that now make Thimbl-CLI usable

--- sitt.py
+++ sitt.py
@@ -1,7 +1,7 @@
-'''sitt.py - Some Interesting Thimbl Tools
-'''
+'''sitt.py - Some Interesting Thimbl Tools '''
 
 import cStringIO
+#import datetime
 import json
 import optparse
 import os
@@ -13,35 +13,34 @@ import time
 stack = []
 
 
-def piping(cmd):
-    global stack
-    stack.append(sys.stdin.read())
-    eval(cmd +'()')
-    print stack.pop()
-    
-def explan():
-    '''Extract the plan from the input'''
-    global stack
-    m = re.search('^.*?Plan:\s*(.*)', stack.pop(), re.M + re.S)
-    stack.append(m.group(1))
+def create(address, bio, name, website, mobile, email):
+    'Create data given user information'
 
-def jencode():
-    '''Encode top of stack to json'''
-    global stack
-    t1 = stack.pop()
-    t2 = json.loads(t1)
-    stack.append(t2)
+    properties =  {'website' : website, 'mobile' : mobile, 'email' : email }
+    plan = { 'address' : address, 'name' : name, 'messages' : [],
+             'replies' : {},'following' : [], 'properties' : properties}
+    data = { 'me' : address, 'plans' : { address : plan }}
+    return data
 
-def jextract():
-    '''Extract a key from a json object on the stack'''
-    global stack
-    key = stack.pop()
-    j = stack.pop()
-    #print t1
-    t2 = json.dumps(t1, indent = 4)
-    stack.append(t2)
+
+
+def myplan(data):
+    return data['plans'][data['me']]
+
+
+def post(data, text):
+    'Create a message. Remember to publish() it'
+    timefmt = time(time.strftime('%Y%m%d%H%M%S'))
+    message = { 'time' : timefmt, 'text' : text }
+    myplan(data)['messages'].append(message)
+
+
     
-    
+
+def follow(data, nick, address):
+    myplan(data)['following'].append( { 'nick' : nick, 'address' : address } )
+
+
 def finger_user(user_name):
     '''Try to finger a user, and convert the returned plan into a dictionary
     E.g. j = finger_user("dk@telekommunisten.org")
@@ -55,37 +54,80 @@ def finger_user(user_name):
     j = json.loads(raw)
     return j
 
-def print_messages(messages):
-    for msg in messages:
-        t = str(msg['time'])
-        y, mo, d, h, mi, s = t[:4], t[4:6], t[6:8], t[8:10], t[10:12], t[12:14]
-        print '{0}-{1}-{2} {3}:{4}:{5}'.format(y, mo, d, h, mi, s)
-        print msg['text']
-        print
-def single_finger(user):
-    '''Print the plan for a given user'''
-    j = finger_user(user)
-    #print j
-    #print j
-    #json.dumps(j, indent = 4)
-    print_messages( j['messages'])
+def fetch(data):
+    '''Retrieve all the plans of the people I am following'''
+    for following in myplan(data)['following']:
+        address = following['address']
+        if address == data['me']:
+            print "Stop fingering yourself!"
+            continue
+
+        print 'Fingering ', address
+        try:
+            plan = finger_user(address)
+        except AttributeError:
+            print 'Failed on address. Skipping'
+            continue
+        data['plans'][address] = plan
+    print 'Finished'
+    
+    
 
 
-def add_message(text):
-    plan_filename = os.path.expanduser('~/.plan')
-    plan_text = file(plan_filename, 'r').read()
-    plan = json.loads(plan_text)
-    t = time.strftime('%Y%m%d%H%M%S')
-    t = int(t)
-    message = { 'time' : t , 'text' : text}
-    if not plan.has_key('messages'): plan['messages'] = []
-    plan['messages'].insert(0, message)
-    plan_text = json.dumps(plan)
-    file(plan_filename, 'w').write(plan_text)
-    print "OK"
+def prmess(data):
+    'print messages'
+    for address in data['plans'].keys():
+        plan = data['plans'][address]
+        print address, plan
+        if not plan.has_key('messages'): continue
+        for msg in plan['messages']:
+            print address
+            t = str(msg['time'])
+            y, mo, d, h, mi, s = t[:4], t[4:6], t[6:8], t[8:10], t[10:12], t[12:14]
+            print '{0}-{1}-{2} {3}:{4}:{5}'.format(y, mo, d, h, mi, s)
+            print msg['text']
+            print
 
-def follow():
-    print "FIXME you're following me"
+
+def save(data, filename):
+    'Save data to a file as a json file'
+    j = json.dumps(data)
+    file(filename, 'w').write(j)
+
+def publish(data):
+    save(myplan(data), os.path.expanduser('~/.plan'))
+         
+def load(filename):
+    'Load data from a json file'
+    s = file(filename, 'r').read()
+    return json.loads(s)
+
+#plan_filename = os.path.expanduser('~/.plan')
+#    plan_text = file(plan_filename, 'r').read()
+#    plan = json.loads(plan_text)
+#    plan['messages'].insert(0, message)
+
+
+def XXXmkfiles(json_text):
+    '''Given some text in json format, create directories and files'''
+
+    def make(data, node_name):
+        'Given a data structure, create a file system'
+        if type(data) is dict:
+            for key in data.keys(): 
+                value = data[key]
+                make(value, node_name + "/" + key)
+        elif (t is str) or (t is unicode) or (t is int):
+            file(node_name, "w").write(str(value))
+        elif t is list:
+            True
+            
+                    
+        
+    j =json.loads(json_text)
+    make(j, "XXX")
+    
+    
     
 def main():
     parser = optparse.OptionParser()
